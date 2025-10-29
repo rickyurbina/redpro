bb6632407f5872a5425fb554feb5a5e741f5563b
Ricardo Urbina Najera <rickyurbina@gmail.com>
2025-10-29 13:41:02 -0600

    faltaba el punto y coma

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     echo "Hello Rick";
     echo "<br>esta es una prueba";
-    echo "<br> modificacion desde el sitio web de GitHub para prueba"
+    echo "<br> modificacion desde el sitio web de GitHub para prueba";
 
 ?>
